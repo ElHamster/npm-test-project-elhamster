35394360f889ace9173aa7b241b8a315bcd5c235
RobinSCU <50205066+RobinSCU@users.noreply.github.com>
2020-09-05 17:23:44 +0200

    bla bal

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,7 +1,3 @@
-import React from 'react';
-
 export interface ITestProps {}
 
 export interface ITestState {}
-
-export default class TestComponent extends React.Component<ITestProps, ITestState> {}
